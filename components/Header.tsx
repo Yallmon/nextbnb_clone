cc16423db07138d7b6f2caa19a603e79eb143f03
Kyusik <chlrbtlr30@ajou.ac.kr>
2021-12-09 22:27:26 +0900

    FIX BUG : SVG import issue

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import styles from "../styles/Header.module.scss";
-import AirbnbLogoIcon from "../public/static/svg/logo/logo.svg";
-import AirbnbLogoTextIcon from "../public/static/svg/logo/logo.svg";
+import AirbnbLogoIcon from "../public/static/svg/logo/airbnb_logo.svg";
+import AirbnbLogoTextIcon from "../public/static/svg/logo/airbnb_logo_text.svg";
 
 const Header: React.FC = () => {
     return (
