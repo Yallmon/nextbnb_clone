7abeb47b83d1aaa615844117a3c6ccf95d18edd0
Kyusik <chlrbtlr30@ajou.ac.kr>
2021-12-10 15:55:50 +0900

    header: css design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Header.module.scss";
 import AirbnbLogoIcon from "../public/static/svg/logo/airbnb_logo.svg";
 import AirbnbLogoTextIcon from "../public/static/svg/logo/airbnb_logo_text.svg";
 
 const Header: React.FC = () => {
+    console.log(styles);
     return (
-        <div className="container">
-            <div className="header-logo-wrapper">
-                <AirbnbLogoIcon className="header-logo" />
-                <AirbnbLogoTextIcon />
+        <div className={styles.container}>
+            <Link href="/">
+                <a className={styles["header-logo-wrapper"]}>
+                    <AirbnbLogoIcon className="header-logo" />
+                    <AirbnbLogoTextIcon />
+                </a>
+            </Link>
+            <div className={styles["header-auth-buttons"]}>
+                <button className={styles["header-sign-up-button"]}>
+                    회원가입
+                </button>
+                <button className={styles["header-login-button"]}>
+                    로그인
+                </button>
             </div>
         </div>
 
     );
+    
 }
 
 export default Header;
